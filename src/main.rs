d6a3c2b8315170498e2bb1326a1265e1abd7a722
jugurtha <jbouhadoun@gmail.com>
2020-04-30 18:02:52 +0200

    2.1 - Deployement du projet et entrées sorties

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
-use std::io::{self};
+use std::io::{self,Write};
 
 fn main() -> std::io::Result<()> {
-let stdin = io::stdin();
+let stdin = io::stdin();;
 let mut user_input = String::with_capacity(256);
+print!(">");
+let stdout= io::stdout().flush();
+
 // On prend une référence mutable
 stdin.read_line(&mut user_input)?;
+print!("{}",user_input);
+
 // `?` sert à « propager l'erreur » dans la fonction appellante
 // c'est mieux que de crash avec un unwrap ou expect ;)
 Ok(())
