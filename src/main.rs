215d6755583550ad804f6dbd0b5ab086c3501250
jugurtha <jbouhadoun@gmail.com>
2020-05-09 23:08:26 +0200

    update1.1

--- src/main.rs
+++ src/main.rs
@@ -12,18 +12,15 @@ impl fmt::Display for Job {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "{}   {}", self.job_id, self.job_command)
     }
-}
+} 
 
-pub struct Jobs{
-    pub table : Vec<Job>,
-    pub count : i32
-}
 
 fn main() -> std::io::Result<()> {
     let stdin = io::stdin();
     let stdout = io::stdout();
 
-    let mut jobs = Jobs{table : Vec::new(), count : 0};
+    //let mut jobs = Jobs{table : Vec::new(), count : 0};
+    let mut jobs =Vec::new();
     let mut user_input = String::with_capacity(256);
 
 loop {
@@ -41,9 +38,14 @@ loop {
     }
 
     let command = split_command_with_espace(&user_input);
+    if command.starts_with("minips"){
+        for i in 0..(jobs.len()-1){
+            println!("{}",jobs[i]);
+        }   
+    }
 
     if command.starts_with("back"){
-        // pipe command
+        // ------------------------------ back with pipe command--------------------------------
         if command.contains("|"){
             let commands : Vec<&str> = command.split("|").collect();  
             let first_command = split_command_with_espace(commands[0]);
@@ -74,12 +76,12 @@ loop {
                         .expect("failed to execute command");
             
             let job = Job{job_id : child.id(), job_command : command.clone()}; 
-            jobs.table.push(job);
+            jobs.push(job);
             match child.try_wait() {
-                Ok(Some(res)) => println!("[{}] {} {:?}",jobs.table.len(), child.id(),command ),
+                Ok(Some(res)) => println!("[{}] {} {:?}",jobs.len(), child.id(),command ),
                 //println!("la  [{}] {} {:?}",jobs.table.len(), child.id(), command),
                 Ok(None) => {; 
-                    println!("[{}] {} {:?}",jobs.table.len(), child.id(), command);
+                    println!("[{}] {} {:?}",jobs.len(), child.id(), command);
                 }
                 Err(e) => println!("error attempting to wait: {}", e),   
         }
@@ -88,17 +90,18 @@ loop {
         }
         else 
         {
+            //-----------------back simple command----------------------------
                          //let child : std::process::Child;
             let args : Vec<&str> = command.split(" ").collect();
             let mut child = Command::new(args[1]).args(&args[2..]).spawn()?;         
 
             let job = Job{job_id : child.id(), job_command : command.clone()}; 
-            jobs.table.push(job);
+            jobs.push(job);
             match child.try_wait() {
-                Ok(Some(res)) => println!("[{}] {} {:?}",jobs.table.len(), child.id(),args[1] ),
+                Ok(_res) => println!("[{}] {} {:?}",jobs.len(), child.id(),args[1] ),
                 //println!("la  [{}] {} {:?}",jobs.table.len(), child.id(), command),
                 Ok(None) => {
-                    println!("[{}] {} {:?}",jobs.table.len(), child.id(), args[1]);
+                    println!("[{}] {} {:?}",jobs.len(), child.id(), args[1]);
                 }
                 Err(e) => println!("error attempting to wait: {}", e),   
         }
@@ -109,7 +112,7 @@ loop {
 
 
     else {
-        // pipe command
+        // ----------------------------simple pipe command----------------------------
         if command.contains("|"){
             let commands : Vec<&str> = command.split("|").collect();  
             let first_command = split_command_with_espace(commands[0]);
@@ -144,7 +147,7 @@ loop {
             }
         }
         else
-        //normal command
+        //------------------------------------normal command----------------------------------------------
         {
             let args : Vec<&str> = command.split(" ").collect();
             let status = Command::new(args[0]).args(&args[1..]).status() .expect("failed to execute command");         
